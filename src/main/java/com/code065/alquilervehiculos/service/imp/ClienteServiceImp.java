ea8a166a8349bbaf3cee3ee65f87cd715c68ae49
xmiroc-uoc <xmiroc@uoc.edu>
2026-03-27 12:20:40 +0100

    feat: crear clienteService y su implementación

---
+++ src/main/java/com/code065/alquilervehiculos/service/imp/ClienteServiceImp.java
@@ -0,0 +1,39 @@
+package com.code065.alquilervehiculos.service.imp;
+
+import com.code065.alquilervehiculos.model.Cliente;
+import com.code065.alquilervehiculos.repository.ClienteRepository;
+import com.code065.alquilervehiculos.service.ClienteService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteServiceImp implements ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    public ClienteServiceImp(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    @Override
+    public List<Cliente> listaClientes() {
+        return clienteRepository.findAll();
+    }
+
+    @Override
+    public Optional<Cliente> buscarClientePorId(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    @Override
+    public Cliente guardarCliente(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @Override
+    public void eliminarCliente(Long id) {
+        clienteRepository.deleteById(id);
+    }
+}
